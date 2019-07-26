55f1ab8c450cacd70196b6ef59752af01ad697fc
Lordemax <lordemax2012@gmail.com>
2019-07-26 14:49:00 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ import telebot
 
 # Example of your code beginning
 #           Config vars
-token = os.environ['TELEGRAM_TOKEN']
-some_api_token = os.environ['SOME_API_TOKEN']
+token = os.environ['TELEG_TOKEN']
+api_token = os.environ['API_TOKEN']
 #             ...
 
 # If you use redis, install this add-on https://elements.heroku.com/addons/heroku-redis
