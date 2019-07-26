567a3323dae6ada5f0d53368bd0cf7d626e89da9
Lordemax <lordemax2012@gmail.com>
2019-07-26 15:57:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import telebot
 
 # Example of your code beginning
 #           Config vars
-bot = telebot.TeleBot["TELEG_TOKEN"]
+bot = telebot.TeleBot["811545344:AAHwO9cyHPnSlA7a6SwZvDEtbAlj7GhcieQ"]
 #api_token = os.environ['API_TOKEN']
 #             ...
 
